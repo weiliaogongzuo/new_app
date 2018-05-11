2ade563f5e1689bd999be4f9ef2d8d83cec65ea2
chenxinying <chen_xin_ying@foxmail.com>
2018-05-11 14:03:23 +0800

    bash

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 docker container rm docker-example -f
 docker rmi chenxinying/docker-example –f
 docker build -t chenxinying/docker-example .
-docker run -d --name docker-example -p 80:80 --link mysql:mysql chenxinying/docker-example
+docker run -d --name docker-example -p 80:80 -v ~/docker-project-example/src:/var/www/html --link mysql:mysql chenxinying/docker-example
